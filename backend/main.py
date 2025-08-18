714773a559fba3bad58d875e8c970b9568839446
Michael <michaeljohn_charles@yahoo.com>
2025-08-18 23:56:14 +0100

    docker updates

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
 
 app = FastAPI()
 
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["ai-pply"]
+
 @app.get("/")
 def root():
-    return {"message": "Backend is running!"}
+    return {"message": "AI-PPLY backend running"}
+
